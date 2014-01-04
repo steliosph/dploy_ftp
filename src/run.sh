6a16d421497fdfbe4ca5c690019964fedd8ec0b5
Stelios Philippou <stevoo82+git@gmail.com>
2014-01-04 09:28:31 +0200

    Will not upload or create directories that were not uploaded before

--- src/run.sh
+++ src/run.sh
@@ -3,19 +3,25 @@
 ##### Constants
 
 parameter_file=dploy
-END="----next----"
+EXECUTE="----NEXT----"
+END="----END----"
 
 while read line           
 do
     parameter=${line##*:}
+    if [[ "$END" == "$parameter" ]]; then
+         ./update.sh ${PARAMETERS_ARRAY[@]}
+        break
+    fi
     command=${line%:*} 
     if [[ "name" == "$command" ]]; then
         PARAMETERS_ARRAY=($parameter);
     else
-      if [[ "$END" == "$parameter" ]]; then
+      if [[ "$EXECUTE" == "$parameter" ]]; then
         ./update.sh ${PARAMETERS_ARRAY[@]}
       else
         PARAMETERS_ARRAY=("${PARAMETERS_ARRAY[@]}" "$parameter")
       fi
     fi
 done <$parameter_file
+
--- src/update.sh
+++ src/update.sh
@@ -6,21 +6,31 @@ array=("$@")
 NAME=${array[0]}
 SCHE=${array[1]}
 HOST=${array[2]}
-# TODO Port is not used when connection is establised
 PORT=${array[3]}
 USER=${array[4]}
 PASS=${array[5]}
 LOCAL_PATH=${array[6]}
 REMOTE_PATH=${array[7]}
-# TODO This needs to be changed and passed through the array as well
-# Add all ignore files here 
+SAVE_TIMES_TO_FILE=${array[8]}
+LOCAL_FILE="dates"
+LOCAL_DIR="directories"
+# TODO This needs to be changed and passed through the array as well 
 IGNORE_FILES=(
-"FIRST_FILE_TO_BE_IGNORED"
-"SECOND_FILE_TO_BE_IGNORED"
+"file.php" 
+"src/directory" 
+)
+
+#Hidden files of files or files that are in a ignored folder. NOT YET WORKING
+INCLUDE_FILES=(
+".htacess"
 )
 
 DIRECTORY_ARRAY=()
 FILES_ARRAY=()
+LOCAL_FILE_CONTENT=()
+LOCAL_FILE_CONTENT_TO_BE_COMPARED=()
+LOCAL_DIR_CONTENT=()
+LOCAL_FIR_CONTENT_TO_BE_COMPARED=()
 
 ##### Functions
 
@@ -28,14 +38,14 @@ loop_trough_directories() {
   for dir in $1
     do
       if [ -d "$dir" ]; then
-         echo -e "\e[0;34m Processing $dir directory..."
+         #echo -e "\e[0;34m Processing $dir directory..."
+         LOCAL_DIR_CONTENT=("${LOCAL_DIR_CONTENT[@]}" "$dir")
+
          for dir_loop in "$dir"*
           do
             check_for_ignored_files $dir_loop
             retval=$?
-            if [ "$retval" == 0 ]; then
-              echo  -e "\e[0;31m  Ignoring $dir_loop dir/file... "
-            else 
+            if [ "$retval" == 1 ]; then
               if [ -d "$dir_loop" ]; then
                 check_if_directory_does_not_exist $dir_loop
                 loop_trough_directories "$dir_loop"/
@@ -43,31 +53,14 @@ loop_trough_directories() {
               if [ -f $dir_loop ]; then
                 loop_through_file $dir_loop
               fi
+           # else
+               #echo  -e "\e[0;31m  Ignoring $dir_loop dir/file... "
+ 
             fi
           done
       fi
   done
 }
-
-loop_through_file() {
-  for file in $1
-    do
-      if [ -f $file ]; then
-        MODDATE=$(date -r $file +%F\ %T )
-        remote_file=${file:${#LOCAL_PATH}:${#file}}
-        echo -e "\t\033[32m Processing $remote_file file... "$MODDATE
-        check_if_file_does_not_exist $remote_file
-      fi
-  done
-}
-
-check_if_file_does_not_exist() {
-  file=$1
-  if [ $(contains "${FILES_ARRAY[@]}" "$file") == "n" ]; then
-    FILES_ARRAY=("${FILES_ARRAY[@]}" "$file")
-  fi
-}
-
 
 check_for_ignored_files() { 
   for var in "${IGNORE_FILES[@]}"
@@ -83,10 +76,11 @@ check_if_directory_does_not_exist() {
   dir=$1
   directory=${dir:${#LOCAL_PATH}:${#dir}}
 
-  if [ $(contains "${DIRECTORY_ARRAY[@]}" "$directory") == "n" ]; then
+  contains "${DIRECTORY_ARRAY[@]}" "$directory"
+  retval=$?
+  if [ "$retval" == 1 ]; then
     DIRECTORY_ARRAY=("${DIRECTORY_ARRAY[@]}" "$directory")
   fi
-
 }
 
 contains() {
@@ -94,17 +88,46 @@ contains() {
     local value=${!n}
     for ((i=1;i < $#;i++)) {
         if [ "${!i}" == "${value}" ]; then
-            echo "y"
             return 0
         fi
     }
-    echo "n"
     return 1
+}
+
+loop_through_file() {
+  for file in $1
+    do
+      if [ -f $file ]; then
+        MODDATE=$(date -r $file +%F\ %T )
+        remote_file=${file:${#LOCAL_PATH}:${#file}}
+        time_created="$LOCAL_PATH$remote_file : $MODDATE"
+        LOCAL_FILE_CONTENT=("${LOCAL_FILE_CONTENT[@]}" "$time_created")
+        check_if_file_does_not_exist $remote_file
+      fi
+  done
+}
+
+check_if_file_does_not_exist() {
+  file=$1
+  contains "${FILES_ARRAY[@]}" "$file"
+  retval=$?
+
+  if [ $retval == 0 ]; then
+    FILES_ARRAY=("${FILES_ARRAY[@]}" "$file")
+  fi
+}
+
+## Read Files for directories
+read_file_for_directories () {
+  if [ -f "$LOCAL_DIR" ]; then
+    IFS=$'\r\n' LOCAL_DIR_CONTENT_TO_BE_COMPARED=($(cat $LOCAL_DIR))
+  fi
 }
 
 connect_to_ftp_and_create_directories() { 
 ftp -inv $HOST <<EOF
   user $USER $PASS
+  cd $REMOTE_PATH
   $(create_directories)
   quit
 EOF
@@ -113,7 +136,12 @@ EOF
 create_directories() {
   for directory in "${DIRECTORY_ARRAY[@]}" 
     do
-      #echo "status" 
+      is_directory_already_created "$directory"
+      retval=$?
+    
+      if [ "$retval" == 1 ];then
+        continue
+      fi
       last_part=${directory##*/}
       IFS='/' read -a array <<< "$directory"
       arraysize=${#array[@]}
@@ -123,18 +151,37 @@ create_directories() {
         do
           if [ "$COUNTER" == "$arraysize" ]; then
             echo "mkdir ${array[${#array[@]}-1]}"
+            break
           else
             echo "cd $dir"
           fi
           COUNTER=$(expr $COUNTER + 1)
         done
       echo "cd /"
+      echo "cd $REMOTE_PATH"
   done
+}
+
+is_directory_already_created () {
+  for directory in "${LOCAL_DIR_CONTENT_TO_BE_COMPARED[@]}"
+  do
+    if [ "$LOCAL_PATH$1/" == "$directory" ];then
+      return 1
+    fi
+  done
+  return 0
+}
+
+read_file_for_date () {
+  if [ -f "$LOCAL_FILE" ]; then
+    IFS=$'\r\n' LOCAL_FILE_CONTENT_TO_BE_COMPARED=($(cat $LOCAL_FILE))
+  fi
 }
 
 connect_to_ftp_and_upload_files() {
 ftp -inv $HOST <<EOF
   user $USER $PASS
+  cd $REMOTE_PATH
   $(upload_files)
   quit
 EOF
@@ -143,24 +190,90 @@ EOF
 upload_files() { 
   for file in "${FILES_ARRAY[@]}"
     do
-      echo "put $LOCAL_PATH$file $file"
+      file_not_found="true"
+      for file_date in "${LOCAL_FILE_CONTENT_TO_BE_COMPARED[@]}"
+      do
+        date_part=${file_date##*: }
+        file_part=${file_date% :*}
+        if [ "$file_part" == "$LOCAL_PATH$file" ]; then
+            file_not_found="false"
+            mod_date=$(date -r $LOCAL_PATH$file +%F\ %T )
+            mod_date_sec=$(date -d "$mod_date" +%s )
+            date_part_sec=$(date -d "$date_part" +%s)
+            if (("$date_part_sec" < "$mod_date_sec")); then
+              echo "put $LOCAL_PATH$file $file"
+              break;
+            fi
+        fi
+      done
+      if [ $file_not_found == "true" ]; then
+        echo "put  $LOCAL_PATH$file $file"
+      fi
   done
 }
 
+write_to_file () { 
+  #echo "Writing last edited dates to file"
+  if [ ! -f "$LOCAL_FILE" ] ; then
+    # Check if file exists if not create the file
+    touch "$LOCAL_FILE"
+  else
+    > "$LOCAL_FILE"
+  fi
+  for line in "${LOCAL_FILE_CONTENT[@]}"
+  do
+    echo $line
+  done >> "$LOCAL_FILE"
+}
+
+write_to_directories () { 
+  #echo "Writing last edited directories to file"
+  if [ ! -f "$LOCAL_DIR" ] ; then
+    # Check if file exists if not create the file
+    touch "$LOCAL_DIR"
+  else
+    > "$LOCAL_DIR"
+  fi
+  for line in "${LOCAL_DIR_CONTENT[@]}"
+  do
+    echo $line
+  done >> "$LOCAL_DIR"
+}
 
 ##### Main
 T="$(date +%s)"
 
+echo -e "\033[1;33;44m--------------------------------------------------\033[0m"
+echo -e "\033[1;33;44m----------- RUNNING THROUGH DIRECTORIES ----------\033[0m"
+echo -e "\033[1;33;44m--------------------------------------------------\033[0m"
+
 loop_trough_directories $LOCAL_PATH
 loop_time="$(($(date +%s)-T))"
 
-#echo ${DIRECTORY_ARRAY[@]}
+echo -e "\033[1;33;44m--------------------------------------------------\033[0m"
+echo -e "\033[1;33;44m-------- CREATING NON EXISTING DIRECTORIES -------\033[0m"
+echo -e "\033[1;33;44m--------------------------------------------------\033[0m"
+
+read_file_for_directories
 connect_to_ftp_and_create_directories
 create_directories="$(($(date +%s)-T))"
 
+echo -e "\033[1;33;44m--------------------------------------------------\033[0m"
+echo -e "\033[1;33;44m---------- UPLOADING & OVERWRITING FILES ---------\033[0m"
+echo -e "\033[1;33;44m--------------------------------------------------\033[0m"
+
+read_file_for_date
 connect_to_ftp_and_upload_files
 upload_files="$(($(date +%s)-T))"
 
+echo -e "\033[1;33;44m--------------------------------------------------\033[0m"
+echo -e "\033[1;33;44m------------ WRITING TO EXISTING FILES -----------\033[0m"
+echo -e "\033[1;33;44m--------------------------------------------------\033[0m"
+
+if [ $SAVE_TIMES_TO_FILE == "true" ]; then
+  write_to_file
+  write_to_directories
+fi
 echo ;
 
 echo "Execution time"
